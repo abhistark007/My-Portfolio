0dc2da48a68d1de3ef93d8162fa5321926ff1431
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2024-03-17 10:53:05 +0530

    Added boiler plate code for HomePage and ErrorPage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
+import HomePage from './pages/HomePage';
+import ErrorPage from './pages/ErrorPage';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App bg-red-600">
-      hellow
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='*' element={<ErrorPage/>}/>
+      </Routes>
     </div>
   );
 }
